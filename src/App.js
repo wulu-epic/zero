5c5f09befd1e4e075a7d9e350bb4cdfe865565f7
coolest person in the world <70172469+wulu-epic@users.noreply.github.com>
2022-11-10 22:22:21 +0000

    fixed ugly

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from "react";
 import "./index.css";
 
 import Home from "./routes/Home";
-import Faq from "./routes/Faq";
-import Contact from "./routes/Contact";
-import Purchase from "./routes/Purchase";
-
-import {Route, Routes} from "react-router-dom";
-
 
 function App() {
   return (
