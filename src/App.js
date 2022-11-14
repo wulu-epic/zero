e1d240528826bc860226361cb59e014813c348e8
coolest person in the world <70172469+wulu-epic@users.noreply.github.com>
2022-11-14 20:35:38 +0000

    cum

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./routes/Home";
 function App() {
   return (
     <Home/>
+    
   );
 }
 
--- src/components/HomeReal.js
+++ src/components/HomeReal.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import {Link} from "react-scroll"
 import "./Home.css";
 import chlogo from "../assets/temporary.PNG";
+import wave from "../assets/wave.png"
 
 
 
@@ -13,15 +14,20 @@ const HomeReal = () => {
 
   return (
     <div className="home" id="home">
+    
       <div className="mask">
+      
         <h1>Welcome to</h1>
         <h2>ZeroWare</h2>
         <img className="image" src={chlogo}></img>
+
         <div className="buttons">
           <button className="purchase-button"><Link to="purchase" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Purchase</Link></button>
           <button className="readme"><Link to="features" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Features</Link></button>
         </div>
       </div>
+      <img className="wave" src={wave}></img>
+
     </div>
   );
 };
